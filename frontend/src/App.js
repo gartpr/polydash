49e2cf5a7475dc1d578d9d2ef0167bbb29b86d90
gartpr <4jetty@comcast.net>
2023-11-04 15:24:22 -0700

    default homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,8 +10,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Landing/ >}>
-            <Route path="home" element={<Home />} />
+          <Route path="/" element={<Home/ >}>
             <Route path="deliveryorder" element={<DeliveryOrder />}/>
           </Route>
         </Routes>
