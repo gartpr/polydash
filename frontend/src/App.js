471c5bf91890630451d1cfacb0fa595b8ba97469
HanYu Wu <hanyuwu04@gmail.com>
2023-11-14 13:15:55 -0800

    Moved BrowserRouter from App.js to index.js to ensure a single routing context for the entire application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import DeliveryPage from './Pages/DeliveryPage.js';
 import OrderPage from './Pages/OrderPage.js';
 import SellerPage from './Pages/SellerPage.js';
@@ -7,22 +7,19 @@ import Home from './Pages/Home.js';
 import OrderForm from './Pages/OrderForm.js';
 import RestaurantMenu from './Pages/RestaurantMenu.js';
 import RegisterAndLogin from './Pages/RegisterAndLogin.js';
-//import { Button } from '@chakra-ui/react';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/delivery" element={<DeliveryPage />} />
-          <Route path="/order" element={<OrderPage />} />
-          <Route path="/seller" element={<SellerPage />} />
-          <Route path="/order/form" element={<OrderForm />} />
-          <Route path="/order/menu" element={<RestaurantMenu />} />
-          <Route path="/signin" element={<RegisterAndLogin />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/delivery" element={<DeliveryPage />} />
+        <Route path="/order" element={<OrderPage />} />
+        <Route path="/seller" element={<SellerPage />} />
+        <Route path="/order/form" element={<OrderForm />} />
+        <Route path="/order/menu" element={<RestaurantMenu />} />
+        <Route path="/signin" element={<RegisterAndLogin />} />
+      </Routes>
     </div>
   );
 }
