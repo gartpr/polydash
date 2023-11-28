5a6f0b97c520b73a5f3503000b6b0399b6bdf418
amanikonda123 <adityamanikonda123@gmail.com>
2023-11-28 00:03:32 -0800

    added new stuff

--- frontend/src/Components/SellerRequest.jsx
+++ frontend/src/Components/SellerRequest.jsx
@@ -7,35 +7,52 @@ import {
   Accordion,
   Box,
   Text,
-  Button,
+  Select,
   Flex,
 } from '@chakra-ui/react';
 import SellerRequestItem from './SellerRequestItem';
 
-const SellerRequest = ({ order, onDenyOrder, onApproveOrder, isPastOrder }) => {
+const SellerRequest = ({ order, onUpdateOrderStatus, isPastOrder }) => {
+  const handleStatusChange = (newStatus) => {
+    onUpdateOrderStatus(order.id, newStatus);
+  };
+
   return (
     <AccordionItem>
       <h2>
         <AccordionButton>
           <Box flex="1" textAlign="left">
-            <Text mt={2} fontWeight="semibold">{order.title} - {order.status}</Text>
+            <Text as='i' mt={2} fontWeight="semibold">{order.title} - Status: {order.status}</Text>
           </Box>
           <AccordionIcon />
         </AccordionButton>
       </h2>
       <AccordionPanel pb={4}>
-        <Text fontWeight="bold">{order.restaurantName}</Text> 
-        <Text mt= {2}>Customer Name: {order.customerName}</Text>
+      <Text fontWeight="bold">Order Information:</Text>
+        {/* <Text mt= {2}>Restaurant Name: {order.restaurant.name}</Text> 
+        <Text mt= {2}>Customer Name: {order.user.name}</Text> */}
+        <Text mt= {2}>Customer Email: {order.user.email}</Text>
+        <Text mt= {2}>Delivery Address: {order.address}</Text>
+        <Text mt= {2}>Payment Method: {order.paymentInfo}</Text>
         <Text mt={2}>Total Price: ${order.totalPrice}</Text>
+        <Text mt={2}>Additional Comments: {order.comments}</Text>
         <Accordion allowMultiple width="full" fontSize="lg">
-          {order.items.map((item) => (
-            <SellerRequestItem key={item.itemId} item={item} />
+          {order.items.map((item, index) => (
+            <SellerRequestItem key={item.itemId} item={item} index={index} />
           ))}
         </Accordion>
         {!isPastOrder && (
-          <Flex justifyContent="flex-end" mt={4} pr={4}>
-            <Button colorScheme="green" onClick={onApproveOrder} mr={2}>Approve</Button>
-            <Button colorScheme="red" onClick={onDenyOrder}>Deny</Button>
+          <Flex justifyContent="space-between" mt={4} alignItems="center">
+            <Box>
+              <Text fontWeight="bold">Update Status:</Text>
+              <Select onChange={(e) => onUpdateOrderStatus(order.id, e.target.value)} value={order.status}>
+                <option value="Received">Received</option>
+                <option value="Confirmed">Confirmed</option>
+                <option value="Preparing">Preparing</option>
+                <option value="Out for Delivery">Out for Delivery</option>
+                <option value="Cancelled">Cancelled</option>
+              </Select>
+            </Box>
           </Flex>
         )}
       </AccordionPanel>
