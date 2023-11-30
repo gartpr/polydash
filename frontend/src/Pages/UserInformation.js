160f3f30ead22796457b76d149420050b88169ee
HanYu Wu <hanyuwu04@gmail.com>
2023-11-30 11:35:34 -0800

    Added userinformation frontend and css styling

--- frontend/src/Pages/UserInformation.js
+++ frontend/src/Pages/UserInformation.js
@@ -3,128 +3,109 @@ import { createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthP
 import { useNavigate } from 'react-router-dom';
 import { database } from '../firebase-config';
 import { db } from '../firebase-config';
-import './RegisterAndLogin.css';
+import './UserInformation.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGooglePlusG } from '@fortawesome/free-brands-svg-icons';
 import { Link } from 'react-router-dom';
 import { collection, addDoc } from "firebase/firestore"
 
+// ... (existing imports)
+
 function UserInformation() {
-  const [isRightPanelActive, setRightPanelActive] = useState(true);
-  const history = useNavigate();
-
-  const handlePanelChange = () => {
-    setRightPanelActive(!isRightPanelActive);
-  };
-
-  const handleGoogleSignIn = async (e) => {
-    e.preventDefault(); // Prevent the default behavior of the event
-    const provider = new GoogleAuthProvider();
-    try {
-      const result = await signInWithPopup(database, provider);
-      console.log(result, 'Google Sign-In success');
-      
-      history('../');
-    } catch (error) {
-      console.error(error, 'Google Sign-In error');
-    }
-  };
+    const history = useNavigate();
   
-
-  const handleSubmit = async (e, type) => {
-    e.preventDefault();
-    const email = e.target.email.value;
-    const password = e.target.password.value;
-
-    const userCollectionRef = collection(db,'users')
-    const userDocRef = await addDoc(userCollectionRef,{name: e.target.name.value, email: email});
-    if (type === 'signup') {
-      createUserWithEmailAndPassword(database, email, password)
-        .then(async (data) => {
-          console.log(data, 'authData');
-          //Create user in database
-        
-          history('../');
-        })
-        .catch((err) => {
-          alert(err.code);
-          setRightPanelActive(true);
+    const addUserDocument = (user) => {
+      const userEmail = user.email;
+  
+      // Assuming you have a function to add/update user data
+      addUserDocumentToFirestore(user);
+    };
+  
+    const addUserDocumentToFirestore = (user) => {
+      const usersCollection = db.collection("users");
+  
+      const userEmail = user.email;
+  
+      // Check if the user with the same email already exists
+      usersCollection
+        .where("email", "==", userEmail)
+        .get()
+        .then((querySnapshot) => {
+          if (querySnapshot.empty) {
+            // User doesn't exist, add a new user document
+            usersCollection
+              .add({
+                email: userEmail,
+                name: user.displayName,
+                // Add more user-specific fields as needed
+              })
+              .then((docRef) => {
+                console.log("Document added with ID:", docRef.id);
+              })
+              .catch((error) => {
+                console.error("Error adding document:", error);
+              });
+          } else {
+            console.log("Document with the same email already exists");
+          }
         });
-    } else {
-      signInWithEmailAndPassword(database, email, password)
-        .then((data) => {
-          console.log(data, 'authData');
-          history('../');
-        })
-        .catch((err) => {
-          alert(err.code);
-        });
-    }
-    if (type === 'google') {
-      handleGoogleSignIn();
-    }
-  };
-
-  const handleReset = () => {
-    history("/reset")
-  }
-
-  console.log("Rendering with right panel active:", isRightPanelActive);
-
-  return (
-    <div className={`RegisterAndLogin ${isRightPanelActive ? 'right-panel-active' : ''}`}>
-      <div className={`form-container sign-up-container ${isRightPanelActive ? 'hidden' : ''}`}>
-        <form onSubmit={(e) => handleSubmit(e, 'signup')}>
-          <h1>Create Account</h1>
-          <div className="social-container">
-            <a href="#" className="social" onClick={(e) => handleGoogleSignIn(e)}>
-            <FontAwesomeIcon icon={faGooglePlusG} />
-            </a>
-          </div>
-          <span>or use your email for registration</span>
-          <input type="text" name="name" placeholder="Name" />
-          <input type="email" name="email" placeholder="Email" />
-          <input type="password" name="password" placeholder="Password" />
-          <button>Sign Up</button>
-        </form>
-      </div>
-      <div className={`form-container sign-in-container ${isRightPanelActive ? '' : 'hidden'}`}>
-        <form onSubmit={(e) => handleSubmit(e, 'signin')}>
-          <h1>Sign in</h1>
-          <div className="social-container">
-            <a href="#" className="social" onClick={(e) => handleGoogleSignIn(e)}>
-            <FontAwesomeIcon icon={faGooglePlusG} />
-            </a>
-          </div>
-          <span>or use your account</span>
-          <input type="email" name="email" placeholder="Email" />
-          <input type="password" name="password" placeholder="Password" />
-          <Link to="/reset">
-          <h2 onClick={handleReset}>Forgot your password?</h2>
-          </Link>
-          <button>Sign In</button>
-        </form>
-      </div>
-      <div className="overlay-container">
-        <div className={`overlay ${isRightPanelActive ? 'right-panel-active' : ''}`}>
-          <div className="overlay-panel overlay-right">
-            <h1>{isRightPanelActive ? 'Hello, Friend!' : 'Welcome Back!'}</h1>
-            <p>{isRightPanelActive ? 'Enter your personal details and start the journey with us' : 'To keep connected with us, please login with your personal info'}</p>
-            <button className="ghost" onClick={handlePanelChange}>
-              {isRightPanelActive ? 'Sign Up' : 'Sign In'}
-            </button>
-          </div>
-          <div className={`overlay-panel overlay-left ${isRightPanelActive ? 'right-panel-active' : ''}`}>
-            <h1>{isRightPanelActive ? 'Welcome Back!' : 'Hello, Friend!'}</h1>
-            <p>{isRightPanelActive ? 'To keep connected with us, please login with your personal info' : 'Enter your personal details and start the journey with us'}</p>
-            <button className="ghost" onClick={handlePanelChange}>
-              {isRightPanelActive ? 'Sign In' : 'Sign Up'}
-            </button>
-          </div>
+    };
+  
+    const handleGoogleSignIn = async (e) => {
+      e.preventDefault();
+      const provider = new GoogleAuthProvider();
+      try {
+        const result = await signInWithPopup(database, provider);
+        console.log(result, 'Google Sign-In success');
+  
+        // Assuming you have a function to add/update user data
+        addUserDocument(result.user);
+  
+        history('../');
+      } catch (error) {
+        console.error(error, 'Google Sign-In error');
+      }
+    };
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+  
+      // Extract additional fields
+      const role = e.target.role.value; // Added field
+      const phoneNumber = e.target.phoneNumber.value; // Added field
+  
+      // Firebase collections
+      const userCollectionRef = collection(db, 'users');
+  
+      // Add user data to the users collection
+      await addDoc(userCollectionRef, {
+        role: role,
+        phoneNumber: phoneNumber,
+      });
+  
+      history('../');
+    };
+  
+    return (
+        <div className={"UserInformation"}>
+          <form onSubmit={handleSubmit}>
+            <h1>User Information</h1>
+            <div className="select-box">
+              <select name="role" defaultValue="">
+                <option value="" disabled>
+                  Select Role
+                </option>
+                <option value="customer">Customer</option>
+                <option value="driver">Driver</option>
+                <option value="restaurant">Restaurant</option>
+              </select>
+            </div>
+            <input type="tel" name="phoneNumber" placeholder="Phone Number" />
+            <button>Submit</button>
+          </form>
         </div>
-      </div>
-    </div>
-  );
-}
-
-export default UserInformation;
+      );
+    }
+  
+  export default UserInformation;
+  
